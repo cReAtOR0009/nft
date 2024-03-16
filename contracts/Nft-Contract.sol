944b81ebd009a8f5af1fd14c643f81a09a538999
cReAtOR0009 <epidnugoairdrop@gmail.com>
2024-03-16 11:04:21 +0100

    modified contract to assign contract owner to deployer

--- contracts/Nft-Contract.sol
+++ contracts/Nft-Contract.sol
@@ -1,19 +1,42 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.9;
+// SPDX-License-Identifier: MIT
+pragma solidity  ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-const creatorMintConract is ERC721, Ownable {
-    uint256 public mintprice = 0.5 ether;
+contract Nft-Contract is ERC721, Ownable {
+    uint256 public mintprice = 0.05 ether;
     uint256 public totalSupply;
     uint256 public maxSupply;
-    bool public isMintEnabled
-    mapping(address => uint256) public mintedWallets
+    bool public isMintEnabled;
+    mapping(address => uint256) public mintedWallets;
 
 
-    constructor () payble ERC721("Simple Mint", SIMPLEMINT) {
-        maxSupply = 2
+  constructor() payable ERC721("_Creator", "CRT") Ownable(msg.sender) {
+        maxSupply = 10;
     }
+
+
+    function toggleIsMIntEnabled()  external onlyOwner {
+        isMintEnabled = !isMintEnabled;
+    }
+
+    function setMaxSupply(uint256 maxSupply_) external onlyOwner {
+        maxSupply = maxSupply_;
+    }
+
+    function mint() external payable {
+        require (isMintEnabled, "minting is not enabled for now");
+        require(mintedWallets[msg.sender] < 1, "max mint per wallet number exceeded");
+        require(msg.value == mintprice, "wrong mint value");
+        require(maxSupply > totalSupply, "sold out Already");
+
+        mintedWallets[msg.sender]++;
+        totalSupply++;
+        uint256 tokenId = totalSupply;
+        _safeMint(msg.sender, tokenId);
+    }
+
+
 
 }
